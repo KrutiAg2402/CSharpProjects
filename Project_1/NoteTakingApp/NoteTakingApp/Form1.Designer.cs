dd654b323d1944ce4fba29dd8a84aab91b33dbed
KrutiAg2402 <engg.kya@gmail.com>
2023-03-30 00:49:28 +0530

    Beginner- Note Taking App

---
+++ Project_1/NoteTakingApp/NoteTakingApp/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace NoteTakingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            label1 = new Label();
+            label2 = new Label();
+            txtTitle = new TextBox();
+            txtMessage = new TextBox();
+            dataGridView1 = new DataGridView();
+            btnNew = new Button();
+            btnSave = new Button();
+            btnRead = new Button();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.HotTrack;
+            label1.Location = new Point(22, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Title";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(128, 128, 255);
+            label2.Location = new Point(22, 118);
+            label2.Name = "label2";
+            label2.Size = new Size(153, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Message";
+            // 
+            // txtTitle
+            // 
+            txtTitle.BackColor = Color.PapayaWhip;
+            txtTitle.Font = new Font("Segoe Print", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            txtTitle.Location = new Point(128, 30);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(328, 39);
+            txtTitle.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            txtMessage.BackColor = Color.NavajoWhite;
+            txtMessage.Font = new Font("Segoe Print", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMessage.Location = new Point(181, 112);
+            txtMessage.Multiline = true;
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(275, 224);
+            txtMessage.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.BackgroundColor = Color.Tan;
+            dataGridView1.BorderStyle = BorderStyle.Fixed3D;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Raised;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.Firebrick;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI Emoji", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = Color.Tan;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.Wheat;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.MenuText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.GridColor = Color.DarkGray;
+            dataGridView1.Location = new Point(499, 30);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.SaddleBrown;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.Window;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(212, 306);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // btnNew
+            // 
+            btnNew.BackColor = Color.FromArgb(255, 192, 128);
+            btnNew.Font = new Font("Segoe Print", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnNew.ForeColor = Color.FromArgb(0, 64, 64);
+            btnNew.Location = new Point(22, 381);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(105, 48);
+            btnNew.TabIndex = 5;
+            btnNew.Text = "New";
+            btnNew.UseVisualStyleBackColor = false;
+            btnNew.Click += btnNew_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.FromArgb(255, 192, 128);
+            btnSave.Font = new Font("Segoe Print", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnSave.ForeColor = Color.FromArgb(0, 64, 64);
+            btnSave.Location = new Point(218, 381);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(104, 48);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.BackColor = Color.FromArgb(255, 192, 128);
+            btnRead.Font = new Font("Segoe Print", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnRead.ForeColor = Color.FromArgb(0, 64, 64);
+            btnRead.Location = new Point(411, 381);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(103, 48);
+            btnRead.TabIndex = 7;
+            btnRead.Text = "Read";
+            btnRead.UseVisualStyleBackColor = false;
+            btnRead.Click += btnRead_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.FromArgb(255, 192, 128);
+            btnDelete.Font = new Font("Segoe Print", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnDelete.ForeColor = Color.FromArgb(0, 64, 64);
+            btnDelete.Location = new Point(610, 381);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(103, 48);
+            btnDelete.TabIndex = 8;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = false;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(795, 489);
+            Controls.Add(btnDelete);
+            Controls.Add(btnRead);
+            Controls.Add(btnSave);
+            Controls.Add(btnNew);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtMessage);
+            Controls.Add(txtTitle);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtTitle;
+        private TextBox txtMessage;
+        private DataGridView dataGridView1;
+        private Button btnNew;
+        private Button btnSave;
+        private Button btnRead;
+        private Button btnDelete;
+    }
+}
